70f975994263556c316032e219288024b9c32c73
G <gbouchet7@gmail.com>
2023-01-31 18:47:03 +0000

    feat: new templated function aliasing

---
+++ src/geronimo/system/aliasing.hpp
@@ -0,0 +1,9 @@
+
+#pragma once
+
+#define D_ALIAS_FUNCTION(__new_func_name, __existing_func_name) \
+  template <typename... Args> \
+  auto __new_func_name(Args&&... args) -> decltype(__existing_func_name(std::forward<Args>(args)...)) { \
+    return __existing_func_name(std::forward<Args>(args)...); \
+  }
+
