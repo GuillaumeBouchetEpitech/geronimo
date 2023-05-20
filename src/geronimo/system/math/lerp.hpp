190a0eb023b81dc06794a62e26f76f746f334fe5
G <gbouchet7@gmail.com>
2023-05-20 20:41:38 +0100

    chore: missing file

--- src/geronimo/system/math/lerp.hpp
+++ src/geronimo/system/math/lerp.hpp
@@ -1,10 +1,14 @@
 
 #pragma once
 
+#include "geronimo/helpers/GLMath.hpp"
+
 namespace gero {
 namespace math {
 
-template <typename T> T lerp(T valueA, T valueB, float coef) { return valueA + (valueB - valueA) * coef; }
+template <typename T> T lerp(const T& valueA, const T& valueB, float coef) { return valueA + (valueB - valueA) * coef; }
+
+glm::quat lerp(const glm::quat& valueA, const glm::quat& valueB, float coef) { return glm::slerp(valueA, valueB, coef); }
 
 } // namespace math
 } // namespace gero
